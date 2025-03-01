0855d69873638e5e6c196ba297547fdcb6c5ec94
haimazu <36477174+haimazu@users.noreply.github.com>
2025-03-01 23:07:06 +0200

    Update monitor_slingshot_sessions.sh

--- monitor_slingshot_sessions.sh
+++ monitor_slingshot_sessions.sh
@@ -11,12 +11,12 @@ SERVER_TYPE="OLD-SERVER"
 SCRIPT_NAME="$SERVER_TYPE - [MONITOR-SLINGSHOT] ->"
 
 echo "$(date '+%d-%m-%Y %H:%M:%S,%3N') - TMUX Slingshot Sessions Monitor Started - Monitoring for patterns..."
-send_telegram_message "$SCRIPT_NAME 🟢 <b>TMUX Slingshot Sessions Monitor Started</b> - Monitoring for patterns."
+send_telegram_message "$SCRIPT_NAME" "🟢 <b>TMUX Slingshot Sessions Monitor Started</b> - Monitoring for patterns."
 
 while true; do
     if ! tmux ls &>/dev/null; then
         echo "No tmux sessions found."
-        send_telegram_message "$SCRIPT_NAME ⚠️ <b>Warning</b>: No tmux sessions found."
+        send_telegram_message "$SCRIPT_NAME" "⚠️ <b>Warning</b>: No tmux sessions found."
     else
         PATTERNS=(
             "BAD DECRYPTION!"
@@ -35,7 +35,7 @@ while true; do
                     echo "Pattern detected in session: $session"
                     echo "Matching Line: $matching_line"
                     # Send Telegram notification
-                    send_telegram_message "$SCRIPT_NAME 🚨 <b>Alert</b>: Pattern detected in session <code>$session</code> -> <i>$matching_line</i>"
+                    send_telegram_message "$SCRIPT_NAME" "🚨 <b>Alert</b>: Pattern detected in session <code>$session</code> -> <i>$matching_line</i>"
 
                     # Clear the console
                     tmux send-keys -t "$session clear" C-m
@@ -48,14 +48,14 @@ while true; do
                     tmux clear-history -t "$session"
 
                     echo "Execution Completed: Session $session processed successfully."
-                    send_telegram_message "$SCRIPT_NAME ✅ <b>Execution Completed</b>: Session <code>$session</code> processed successfully."
+                    send_telegram_message "$SCRIPT_NAME" "✅ <b>Execution Completed</b>: Session <code>$session</code> processed successfully."
                 fi
             fi
         done
     fi
 
     echo "$(date '+%d-%m-%Y %H:%M:%S,%3N') - Next check in 30 minutes..."
-    send_telegram_message "$SCRIPT_NAME ⏳ <b>Next Check in 30 Minutes</b>..."
+    send_telegram_message "$SCRIPT_NAME" "⏳ <b>Next Check in 30 Minutes</b>..."
     # Wait for 30 minutes before running again
     sleep 1800
 done
