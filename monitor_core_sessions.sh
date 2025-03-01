d97556a51fd091220d900efbe0e502bbeae72ac0
haimazu <36477174+haimazu@users.noreply.github.com>
2025-03-01 23:05:22 +0200

    Update monitor_core_sessions.sh

--- monitor_core_sessions.sh
+++ monitor_core_sessions.sh
@@ -11,7 +11,7 @@ SERVER_TYPE="OLD-SERVER"
 SCRIPT_NAME="$SERVER_TYPE - [MONITOR-CORE] ->"
 
 echo "$(date '+%d-%m-%Y %H:%M:%S,%3N') - TMUX CORE Monitor Started"
-send_telegram_message "$SCRIPT_NAME 🟢 <b>TMUX CORE Monitor Started</b>"
+send_telegram_message "$SCRIPT_NAME" "🟢 <b>TMUX CORE Monitor Started</b>"
 
 # Function to check and restart Tmux sessions
 check_and_restart() {
@@ -23,10 +23,10 @@ check_and_restart() {
 
     # status = 0 => session exists
     if [[ $status -ne 0 ]] then # check if status == 1
-        tmux new -d -s "$session_name $command"
+        tmux new -d -s "$session_name" "$command"
 
         echo "$(date '+%d-%m-%Y %H:%M:%S,%3N') - Session Restarted: $session_name and now running."
-        send_telegram_message "$SCRIPT_NAME ✅ <b>Session Restarted: </b>: <code>$session_name</code> and now running."
+        send_telegram_message "$SCRIPT_NAME" "✅ <b>Session Restarted: </b>: <code>$session_name</code> and now running."
     fi
 }
 
@@ -41,7 +41,7 @@ declare -A SESSIONS=(
 while true; do
     # Monitor Core Sessions
     for session in "${!SESSIONS[@]}"; do
-        check_and_restart "$session ${SESSIONS[$session]}"
+        check_and_restart "$session" "${SESSIONS[$session]}"
     done
 
     sleep 10  # Check every 10 seconds
