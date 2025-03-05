ee633b77875c619e0d1aefcff470b032dc7edf12
haimazu <36477174+haimazu@users.noreply.github.com>
2025-03-05 19:07:06 +0200

    Update cleanup_tmp_folders.sh

--- cleanup_tmp_folders.sh
+++ cleanup_tmp_folders.sh
@@ -11,19 +11,37 @@ SERVER_TYPE="OLD-SERVER"
 SCRIPT_NAME="$SERVER_TYPE - [CLEANUP] ->"
 
 echo "$(date '+%d-%m-%Y %H:%M:%S,%3N') - Temporary Files Cleanup Started"
-send_telegram_message "$SCRIPT_NAME 🧹 <b>Temporary Files Cleanup Started</b> - Removing old `/tmp` folders."
+send_telegram_message "$SCRIPT_NAME" "🧹 <b>Temporary Files Cleanup Started</b> - Removing old `/tmp` folders."
 
 while true; do
     # Find and delete all /tmp/nuclei* folders older than 2 hours
-    deleted_nuclei=$(find /tmp/ -maxdepth 1 -type d -name 'nuclei*' -mmin +120 -exec rm -rf {} + 2>/dev/null | wc -l)
-    # Find and delete the /tmp/wpscan folder older than 2 hours
-    deleted_wpscan=$(find /tmp/ -maxdepth 1 -type d -name 'wpscan' -mmin +120 -exec rm -rf {} + 2>/dev/null | wc -l)
+    nuclei_count=$(find /tmp/ -maxdepth 1 -type d -name 'nuclei*' -mmin +120 | wc -l)
+    if [[ "$nuclei_count" -gt 0 ]]; then
+        find /tmp/ -maxdepth 1 -type d -name 'nuclei*' -mmin +120 -exec rm -rf {} + 2>/dev/null
+        deleted_nuclei=$nuclei_count
+    else
+        deleted_nuclei=0
+    fi
 
-    echo "$(date '+%d-%m-%Y %H:%M:%S,%3N') - Cleanup Completed"
-    send_telegram_message "$SCRIPT_NAME ✅ <b>Cleanup Completed</b>:  
-    🗑️ Deleted <code>$deleted_nuclei</code> <b>nuclei*</b> folders.  
-    🗑️ Deleted <code>$deleted_wpscan</code> <b>wpscan</b> folder.  
-    ⏳ <i>Next cleanup in 12 hours...</i>"
+    # Find and delete the /tmp/wpscan folder older than 2 hours
+    wpscan_count=$(find /tmp/ -maxdepth 1 -type d -name 'wpscan' -mmin +120 | wc -l)
+    if [[ "$wpscan_count" -gt 0 ]]; then
+        find /tmp/ -maxdepth 1 -type d -name 'wpscan' -mmin +120 -exec rm -rf {} + 2>/dev/null
+        deleted_wpscan=$wpscan_count
+    else
+        deleted_wpscan=0
+    fi
+
+    
+    if [[ "$deleted_nuclei" -gt 0 || "$deleted_wpscan" -gt 0 ]]; then
+        echo "$(date '+%d-%m-%Y %H:%M:%S,%3N') - Cleanup Completed"
+        send_telegram_message "$SCRIPT_NAME" "✅ <b>Cleanup Completed</b>:  
+        🗑️ Deleted <code>$deleted_nuclei</code> <b>nuclei*</b> folders.  
+        🗑️ Deleted <code>$deleted_wpscan</code> <b>wpscan</b> folder."
+    else
+        echo "$(date '+%d-%m-%Y %H:%M:%S,%3N') - Cleanup Skipped: No old temporary folders found."
+        send_telegram_message "$SCRIPT_NAME" "ℹ️ <b>Cleanup Skipped</b>: No old temporary folders found."
+    fi
 
     echo "Next cleanup in 12 hours..."
     send_telegram_message "$SCRIPT_NAME" "⏳ <b>Next cleanup in 12 hours</b>..."
